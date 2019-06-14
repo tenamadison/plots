ad60bdb6790c5718f116d37e0a38119baa3da51b
Tena Madison <tena.madison@wisc.edu>
2019-06-14 14:04:42 -0500

    not sure what's up

--- gdp_plots.py
+++ gdp_plots.py
@@ -1,26 +1,29 @@
+
+import sys #import library that works with CSV files
 import pandas
 # we need to import part of matplotlib
 # because we are no longer in a notebook
 import matplotlib.pyplot as plt
 
-filename='gapminder_gdp_oceania.csv'
+for filename in sys.argv[1:]:
+    # load data and transpose so that country names are
+    # the columns and their gdp data becomes the rows
+    data = pandas.read_csv(filename, index_col = 'country').T
 
-# load data and transpose so that country names are
-# the columns and their gdp data becomes the rows
-data = pandas.read_csv(filename, index_col = 'country').T
+    # create a plot of the transposed data
+    ax = data.plot(title=filename)
 
-# create a plot of the transposed data
-ax = data.plot(title=filename)
+    # display the plot
+    plt.show()
 
-# display the plot
-plt.show()
+    #set some plot
+    ax.set_xlabel("YEar")
+    ax.set_yLabel ("GDP per capita")
+    #set the x locations and lebels
+    ax.set_xticks(range(len(data.index)))
+    ax.set_xtickslabels(data.index, rotation =45)
 
-#set some plot
-ax.set_xlabel("YEar")
-ax.set_yLabel ("GDP per capita")
-#set the x locations and lebels
-ax.set_xticks(range(len(data.index)))
-ax.set_xtickslabels(data.index, rotation =45)
-
-#display
-pit.show()
+    #display the plot
+    split_name=filename.split(".")
+    save_name=split_name[0] + "png"    
+    pit.savefig (save_name)
